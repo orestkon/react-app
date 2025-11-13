1c8ff9c7aeb0a7b4ab4888982de0a5c5f2abbc14
Orestis Konstantinidis <30441437+orestkon@users.noreply.github.com>
2025-11-13 21:47:42 +0200

    Form experiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import Form from "./components/Form";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
-  const handleClose = () => {
-    setAlertVisibility(false);
-  };
   return (
     <>
-      {alertVisible && (
-        <div>
-          <Alert onClose={handleClose}>Hello World</Alert>
-        </div>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+      <div>
+        <Form></Form>
+      </div>
     </>
   );
 }
