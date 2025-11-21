40ec3400713033b0135c00acab2e369829ef7013
Orestis Konstantinidis <30441437+orestkon@users.noreply.github.com>
2025-11-21 18:28:07 +0200

    Zod validation and new categories list.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/ExpenseFilter";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -25,6 +26,16 @@ function App() {
   return (
     <>
       <div>
+        <div className="mb-5">
+          <ExpenseForm
+            onSubmit={(expense) =>
+              setExpenses([
+                ...expenses,
+                { ...expense, id: expenses.length + 1 },
+              ])
+            }
+          />
+        </div>
         <div className="mb-3">
           <ExpenseFilter
             onSelectCategory={(category) => setSelectedCategory(category)}
--- src/expense-tracker/components/ExpenseFilter.tsx
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -1,3 +1,5 @@
+import categories from "../categories";
+
 interface Props {
   onSelectCategory: (category: string) => void;
 }
@@ -5,13 +7,16 @@ interface Props {
 const ExpenseFilter = ({ onSelectCategory }: Props) => {
   return (
     <select
+      id="filter"
       className="form-select"
       onChange={(event) => onSelectCategory(event?.target.value)}
     >
       <option value="">All Categories</option>
-      <option value="Groceries">Groceries</option>
-      <option value="Utilities">Utilities</option>
-      <option value="Entertainment">Entertainment</option>
+      {categories.map((category) => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
     </select>
   );
 };
