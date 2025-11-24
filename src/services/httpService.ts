6aacb8772ca2abf8be0151003617cd9cc7ef9c9f
Orestis Konstantinidis <30441437+orestkon@users.noreply.github.com>
2025-11-25 01:18:35 +0200

    Extracting an http service

---
+++ src/services/httpService.ts
@@ -0,0 +1,40 @@
+import apiClient from "./apiClient";
+
+
+interface Entity {
+    id: number;
+}
+
+class HttpService {
+    endpoint: string;
+
+    constructor(endpoint: string) {
+        this.endpoint = endpoint;
+    }
+
+    getAll<T>() {
+        const controller = new AbortController();
+        const request =apiClient
+              .get<T[]>(this.endpoint, {
+                signal: controller.signal,
+              })
+        return {request, cancel: () => controller.abort()};
+    }
+
+    create<T>(entity: T) {
+        return apiClient.post<T>(this.endpoint, entity);
+    }
+
+    update<T extends Entity>(entity: T) {
+        return apiClient.put<T>(`${this.endpoint}/${entity.id}`, entity);
+    }
+
+    delete(id: number) {
+        return apiClient.delete(`${this.endpoint}/${id}`);
+    }
+    
+}
+
+const create = (endpoint: string) => new HttpService(endpoint);
+
+export default create;
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,33 +1,8 @@
-import apiClient from "./api-client";
+import create from "./httpService";
 
 export interface User {
   id: number;
   name: string;
 }
 
-class UserService {
-
-    getAllUsers() {
-        const controller = new AbortController();
-        const request =apiClient
-              .get<User[]>("users", {
-                signal: controller.signal,
-              })
-        return {request, cancel: () => controller.abort()};
-    }
-
-    addUser(user: User) {
-        return apiClient.post<User>("users", user);
-    }
-
-    updateUser(user: User) {
-        return apiClient.put<User>(`users/${user.id}`, user);
-    }
-
-    deleteUser(id: number) {
-        return apiClient.delete<User>(`users/${id}`);
-    }
-    
-}
-
-export default new UserService();
+export default create("/users");
