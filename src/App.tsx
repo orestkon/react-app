bcd7ec9132ae1ddb37972057ebaeedcbfc2fb34f
Orestis Konstantinidis <30441437+orestkon@users.noreply.github.com>
2025-11-24 22:17:01 +0200

    loading experiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,46 @@
-import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import axios, { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "expense 1", amount: 10, category: "Utilities" },
-    { id: 2, description: "expense 2", amount: 10, category: "Utilities" },
-    { id: 3, description: "expense 3", amount: 10, category: "Utilities" },
-    { id: 4, description: "expense 4", amount: 10, category: "Utilities" },
-  ]);
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => setUsers(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
 
-  const onDelete = (id: number) => {
-    setExpenses(
-      expenses.filter((expense) => (expense.id === id ? null : expense))
-    );
-  };
-
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((expense) => expense.category === selectedCategory)
-    : expenses;
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <div className="mb-5">
-          <ExpenseForm
-            onSubmit={(expense) =>
-              setExpenses([
-                ...expenses,
-                { ...expense, id: expenses.length + 1 },
-              ])
-            }
-          />
-        </div>
-        <div className="mb-3">
-          <ExpenseFilter
-            onSelectCategory={(category) => setSelectedCategory(category)}
-          />
-        </div>
-        <ExpenseList expenses={visibleExpenses} onDelete={onDelete} />
-      </div>
+      {error && <p className="text-danger">{error}</p>}
+      {loading && <div className="spinner-border" role="status"></div>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
